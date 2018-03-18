ddec0c035bc8a398e9d2181cb333b247076f3d13
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-18 19:42:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ function prependKitten (name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0,kittens.length-1)
-  return kittens
+  
+  return kittenskittens.slice(0,kittens.length-1)
 }
 
 function removeFirstKitten() {
